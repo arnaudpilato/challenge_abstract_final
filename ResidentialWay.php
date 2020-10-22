2616ee67e00d08afdf1c472e94ee06b0479b6c67
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-10-23 00:46:06 +0200

    Create ResidentialWay.php

---
+++ ResidentialWay.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . 'HighWay.php';
+
+final class ResidentialWay extends HighWay
+{
+    public function __construct()
+    {
+        $this->nbLane = 2;
+        $this->maxSpeed = 50;
+    }
+
+    public function addVehicle()
+    {
+        $currentVehicle[] = $this;
+    }
+}
