f030aaf22d0339b2ef670006daba7a51c0b41bd5
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-10-26 10:53:52 +0100

    Update ResidentialWay.php

--- ResidentialWay.php
+++ ResidentialWay.php
@@ -2,17 +2,16 @@
 
 require_once __DIR__ . '/HighWay.php';
 
-final class PedestrianWay extends HighWay
+final class ResidentialWay extends HighWay
 {
-    public function __construct()
-    {
-        $this->nbLane = 4;
-        $this->maxSpeed = 130;
+    public function __construct() {
+        $this->nbLane = 2;
+        $this->maxSpeed = 50;
     }
 
     public function addVehicle()
     {
-        if ($this instanceof Bicycle) {
+        if ($this instanceof Vehicle) {
             $currentVehicle[] = $this;
         }
     }
