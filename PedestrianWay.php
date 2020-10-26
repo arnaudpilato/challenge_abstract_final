a90ff85c56dcd2fabbd38fa4a7605ca97def097f
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-10-26 16:24:43 +0100

    Update PedestrianWay.php

--- PedestrianWay.php
+++ PedestrianWay.php
@@ -1,9 +1,20 @@
 <?php
 
-require_once __DIR__ . 'HighWay.php';
+require_once __DIR__ . '/HighWay.php';
 
 final class PedestrianWay extends HighWay
 {
-    protected $nbLane = 1;
-    protected $maxSpeed = 10;
+    public function __construct()
+    {
+        $this->nbLane = 4;
+        $this->maxSpeed = 130;
+    }
+
+    public function addVehicle(Vehicle $vehicle)
+    {
+        if ($vehicle instanceof Bicycle) {
+            $currentVehicle[] = $this;
+        }
+
+    }
 }
