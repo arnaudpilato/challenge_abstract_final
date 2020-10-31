62e20993607274e0f6953d56a7cb7519de1d1ce7
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-10-31 21:57:57 +0100

    Update MotorWay.php

--- MotorWay.php
+++ MotorWay.php
@@ -13,7 +13,7 @@ final class MotorWay extends HighWay
     public function addVehicle(Vehicle $vehicle)
     {
         if ($vehicle instanceof Car) {
-            $currentVehicle[] = $this;
+            $currentVehicles[] = $vehicle;
         }
     }
 }
